a54df8fec2e6fffac368fcfc1bbf252759094982
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-03-11 17:10:40 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+</head>
+<body>
+    <fieldset><legend><h1>Lista de Produtos</h1></legend>
+
+    <table>
+  <caption>
+    Front-end web developer course 2021
+  </caption>
+  <thead>
+    <tr>
+      <th scope="col">Person</th>
+      <th scope="col">Most interest in</th>
+      <th scope="col">Age</th>
+    </tr>
+  </thead>
+  
+</table>
+
+<?php
+include "conexao.php";
+$sql = "SELECT `Nome`,`Quantidade` FROM `produtos`";
+
+?>
+
+</fieldset>
+
+</body>
+</html>
