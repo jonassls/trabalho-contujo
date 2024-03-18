872ac18601c261e776b7395723f75fc96a701462
UrielMontanha <uriel.2022316043@aluno.iffar.edu.br>
2024-03-18 15:50:34 -0300

    fazendo tabela dos produtos

--- index.php
+++ index.php
@@ -15,7 +15,20 @@ $sql = "SELECT `Nome`,`Quantidade` FROM `produtos`";
 <body>
     <fieldset><legend><h1>Lista de Produtos</h1></legend>
 
-
+<table>
+  <tr>
+    <th> Produtos </th>
+    <th> Quantidade produtos </th>
+  </tr>
+  <tr>
+    <td>ppp</td>
+    <td>ddd</td>
+  </tr>
+  <tr>
+    <td>jjj</td>
+    <td>ggg</td>
+  </tr>
+</table>
 
 
   </fieldset>
